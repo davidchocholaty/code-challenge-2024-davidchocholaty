76b101f4377e4bb1838233119c5aaf428df49ffd
David Chocholaty <david.chocholaty@seznam.cz>
2024-04-28 14:33:01 +0200

    Add p2pkh verification
    This commit adds the implementation until the p2pkh verification.

--- run.sh
+++ run.sh
@@ -1,1 +1,11 @@
-# Update this file to run your own code
+#!/bin/bash
+
+#python3 -m venv venv
+#venv/bin/pip install --upgrade pip
+#venv/bin/pip install -r requirements.txt
+#source /venv/bin/activate
+python3 -m venv venv
+venv/bin/pip install --upgrade pip
+venv/bin/pip install -r requirements.txt
+source venv/bin/activate
+python3 main.py --mempool=mempool > output.txt
